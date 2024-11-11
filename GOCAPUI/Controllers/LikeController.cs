15dd059b88d0dfd7b12c5801f95ec765d57aa4e5
TranNgocChi <brightsuntnc2003@gmail.com>
2024-11-11 23:53:49 +0700

    Init UI

---
+++ GOCAPUI/Controllers/LikeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GOCAPUI.Controllers
+{
+    public class LikeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
