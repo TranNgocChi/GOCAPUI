f3c9bfbdd86682dd64f006f1edb9d4ad483c5a05
macnhutoan <toannightfury62@gmail.com>
2024-11-14 22:02:57 +0700

    admin user

--- GOCAPUI/Models/DTO/Post/PostCreationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GOCAPUI.Models.DTO.Post;
-
-public class PostCreationModel
-{
-    public string? Content { get; set; }
-    public Guid UserId { get; set; }
-    public ICollection<IFormFile>? MediaFiles { get; set; }
-}
--- GOCAPUI/Models/ResponseJson/UserRespose.cs
+++
@@ -1,9 +0,0 @@
-﻿using GOCAPUI.Models.DTO.User;
-
-namespace GOCAPUI.Models.ResponseJson
-{
-    public class UserRespose
-    {
-        public IEnumerable<UserModel>? Value { get; set; }
-    }
-}
